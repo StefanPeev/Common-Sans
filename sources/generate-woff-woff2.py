457f1bf348690e58fa7a87761fc40ff227022f29
unknown <context.bg@gmail.com>
2025-03-31 03:45:47 +0300

    Update to v.1.151

---
+++ sources/generate-woff-woff2.py
@@ -0,0 +1,412 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/CommonSans-CondensedThin.ttf'
+woffPath = './web/CommonSans-CondensedThin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-CondensedLight.ttf'
+woffPath = './web/CommonSans-CondensedLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-CondExtLt.ttf'
+woffPath = './web/CommonSans-CondExtLt.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-Condensed.ttf'
+woffPath = './web/CommonSans-Condensed.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-CondensedMedium.ttf'
+woffPath = './web/CommonSans-CondensedMedium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-CondensedSemiBold.ttf'
+woffPath = './web/CommonSans-CondensedSemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/CommonSans-CondensedBold.ttf'
+woffPath = './web/CommonSans-CondensedBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-CondensedExtraBold.ttf'
+woffPath = './web/CommonSans-CondensedExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-CondensedBlack.ttf'
+woffPath = './web/CommonSans-CondensedBlack.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-Thin.ttf'
+woffPath = './web/CommonSans-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-Light.ttf'
+woffPath = './web/CommonSans-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-ExtraLight.ttf'
+woffPath = './web/CommonSans-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-Regular.ttf'
+woffPath = './web/CommonSans-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-Medium.ttf'
+woffPath = './web/CommonSans-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+    
+srcPath  = './ttf/CommonSans-SemiBold.ttf'
+woffPath = './web/CommonSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-Bold.ttf'
+woffPath = './web/CommonSans-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-ExtraBold.ttf'
+woffPath = './web/CommonSans-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-Black.ttf'
+woffPath = './web/CommonSans-Black.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+srcPath  = './ttf/CommonSans-ExpandedThin.ttf'
+woffPath = './web/CommonSans-ExpandedThin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-ExpandedLight.ttf'
+woffPath = './web/CommonSans-ExpandedLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-ExpandedExtraLight.ttf'
+woffPath = './web/CommonSans-ExpandedExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-Expanded.ttf'
+woffPath = './web/CommonSans-Expanded.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-ExpandedMedium.ttf'
+woffPath = './web/CommonSans-ExpandedMedium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-ExpandedSemiBold.ttf'
+woffPath = './web/CommonSans-ExpandedSemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/CommonSans-ExpandedBold.ttf'
+woffPath = './web/CommonSans-ExpandedBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-ExpandedExtraBold.ttf'
+woffPath = './web/CommonSans-ExpandedExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-ExpandedBlack.ttf'
+woffPath = './web/CommonSans-ExpandedBlack.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/CommonSans-CondensedThin.ttf'
+woffPath = './web/CommonSans-CondensedThin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-CondensedLight.ttf'
+woffPath = './web/CommonSans-CondensedLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-CondExtLt.ttf'
+woffPath = './web/CommonSans-CondExtLt.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-Condensed.ttf'
+woffPath = './web/CommonSans-Condensed.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-CondensedMedium.ttf'
+woffPath = './web/CommonSans-CondensedMedium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-CondensedSemiBold.ttf'
+woffPath = './web/CommonSans-CondensedSemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)
+    
+srcPath  = './ttf/CommonSans-CondensedBold.ttf'
+woffPath = './web/CommonSans-CondensedBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-CondensedExtraBold.ttf'
+woffPath = './web/CommonSans-CondensedExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-CondensedBlack.ttf'
+woffPath = './web/CommonSans-CondensedBlack.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-Thin.ttf'
+woffPath = './web/CommonSans-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-Light.ttf'
+woffPath = './web/CommonSans-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-ExtraLight.ttf'
+woffPath = './web/CommonSans-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-Regular.ttf'
+woffPath = './web/CommonSans-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-Medium.ttf'
+woffPath = './web/CommonSans-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+
+    
+srcPath  = './ttf/CommonSans-SemiBold.ttf'
+woffPath = './web/CommonSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-Bold.ttf'
+woffPath = './web/CommonSans-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-ExtraBold.ttf'
+woffPath = './web/CommonSans-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-Black.ttf'
+woffPath = './web/CommonSans-Black.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+srcPath  = './ttf/CommonSans-ExpandedThin.ttf'
+woffPath = './web/CommonSans-ExpandedThin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-ExpandedLight.ttf'
+woffPath = './web/CommonSans-ExpandedLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-ExpandedExtraLight.ttf'
+woffPath = './web/CommonSans-ExpandedExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-Expanded.ttf'
+woffPath = './web/CommonSans-Expanded.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-ExpandedMedium.ttf'
+woffPath = './web/CommonSans-ExpandedMedium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)
+
+srcPath  = './ttf/CommonSans-ExpandedSemiBold.ttf'
+woffPath = './web/CommonSans-ExpandedSemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)
+    
+srcPath  = './ttf/CommonSans-ExpandedBold.ttf'
+woffPath = './web/CommonSans-ExpandedBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-ExpandedExtraBold.ttf'
+woffPath = './web/CommonSans-ExpandedExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CommonSans-ExpandedBlack.ttf'
+woffPath = './web/CommonSans-ExpandedBlack.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woffPath)    
